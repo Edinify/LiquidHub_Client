847077ea2eda467a17a99501d5defd8c7f05e0d6
xeyyamomarov <xeyyamomarov2014@gmail.com>
2023-12-22 10:06:29 +0400

    /

--- src/pages/StakePage/components/StakeModal.jsx
+++ src/pages/StakePage/components/StakeModal.jsx
@@ -1,30 +1,39 @@
-import React from 'react'
-import "./stake-modal.css"
-import {ReactComponent as CloseIcon} from "../../../assets/stake/x-close.svg"
+import React from "react";
+import "./stake-modal.css";
+import { ReactComponent as CloseIcon } from "../../../assets/stake/x-close.svg";
 
-const StakeModal = ({setOpenModal}) => {
+const StakeModal = ({ setOpenModal }) => {
   return (
-    <div className='stake-modal'>
-        <div className="stake-modal-container">
-        <div className='stake-modal-context'>
-            <div className="modal-header">
-               <div className="header-context">
-                <h3>Stake KUJI</h3>
-                <p>Please note there is a <strong>2 week </strong>unstaking time.</p>
-               </div>
-               <div onClick={()=>setOpenModal(false)} className="close-icon">
-                    <CloseIcon/>
-               </div>
+    <div className="stake-modal">
+      <div className="stake-modal-container">
+        <div className="stake-modal-context">
+          <div className="modal-header">
+            <div className="header-context">
+              <h3>Stake KUJI</h3>
+              <p>
+                Please note there is a <strong>2 week </strong>unstaking time.
+              </p>
             </div>
-            <div className="modal-input">
-                <h4>Amount</h4>
-                <input type="text" />
+            <div onClick={() => setOpenModal(false)} className="close-icon">
+              <CloseIcon />
             </div>
+          </div>
+          <div className="modal-input">
+            <h4>Amount</h4>
+            <input type="number" placeholder="KUJI" />
+          </div>
+          <div className="max">
+            <p>
+              Max: <span>0.000</span>
+            </p>
+          </div>
+          <div className="stake-btn">
+            <button>Stake KUJI</button>
+          </div>
         </div>
-
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default StakeModal
+export default StakeModal;
